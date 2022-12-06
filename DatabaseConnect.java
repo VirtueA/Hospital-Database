7de74149a2da8abcea3e77269783bc414cc82f82
Rafael Mortel Gayahan <74213299+Rafael-Gayahan@users.noreply.github.com>
2022-12-05 22:06:24 -0600

    Add files via upload

--- DatabaseConnect.java
+++ DatabaseConnect.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.sql.*;
 import java.util.Date;
 
@@ -45,6 +46,7 @@ public class DatabaseConnect {
 
     //function to insert a patient
     //date needs to be in form MM/DD/YYYY
+    //NEED THIS
     public static void insertPatient(Connection con, String name, String SSN, String ID, String date) throws SQLException {
         Date patientDOB = new Date(date);
         java.sql.Date sqlDate = new java.sql.Date(patientDOB.getTime());
@@ -58,6 +60,7 @@ public class DatabaseConnect {
 
     //function to delete a patient
     //deletes patient by finding the patient's ssn
+    //NEED THIS
     public static void deletePatient(Connection con, String SSN) throws SQLException {
         PreparedStatement ptDelete = con.prepareStatement("DELETE FROM PATIENTS WHERE SSN = ?");
         ptDelete.setString(1, SSN);
@@ -65,111 +68,120 @@ public class DatabaseConnect {
     }
 
     //finds patient using their patientID
-    public static ResultSet findPatient(Connection con, String patientID) throws SQLException {
+    //NEED THIS
+    public static String findPatient(Connection con, String patientID) throws SQLException {
         PreparedStatement findPt = con.prepareStatement("SELECT PatientID, SSN, PatientName, DOB FROM patients where PatientID = ?");
         findPt.setString(1, patientID);
-        ResultSet result = findPt.executeQuery();
-        return result;
-    }
-
-    public static void updateDoctorPhone(Connection con, String docID, String newPhoneNum) throws SQLException {
-        PreparedStatement updateDoctorPhone = con.prepareStatement("UPDATE DOCTORS SET PhoneNum = ? where doctorID = ? ");
-        updateDoctorPhone.setString(1, newPhoneNum);
-        updateDoctorPhone.setString(2, docID);
-        updateDoctorPhone.execute();
-    }
-
-    //return all the doctors with a given amount of years of experience
-    public static ResultSet retrieveDoctorsExpereience(Connection con, int yearsExperience) throws SQLException {
-        PreparedStatement retrieveDocs = con.prepareStatement("Select DoctorID, FullName, DeptName FROM doctors Where doctors.YrsExperience >= ?");
-        retrieveDocs.setInt(1, yearsExperience);
-        //getting the result set
-        ResultSet results = retrieveDocs.executeQuery();
-        //returning the result set
-        return results;
-
-    }
-
-    public static ResultSet GenderDocs(Connection con, String gender) throws SQLException {
-        PreparedStatement retrieveMaleDocs = con.prepareStatement("SELECT doctorID, FullName FROM Doctors Where gender = ?");
-        retrieveMaleDocs.setString(1, gender);
-        ResultSet result = retrieveMaleDocs.executeQuery();
-
-        while (result.next()) {
-            String doctorID = result.getString("doctorID");
-            String FullName = result.getString("FullName");
-
-            //printing results
-            System.out.println(doctorID + "|" + FullName);
+        ResultSet results = findPt.executeQuery();
+        String result = "Patient ID | SSN | Patient Name | DOB \n";
+        if(!results.isBeforeFirst()) {
+            return "Error: No matching data found";
         }
-        return result;
+        else {
+            while(results.next()) {
+                String ptID = results.getString("PatientID");
+                String ssn = results.getString("SSN");
+                String ptName = results.getString("PatientName");
+                java.sql.Time dob = results.getTime("DOB");
+
+                result += ptID + " | " + ssn + " | " + ptName + " | " + dob + " \n";
+            }
+            return result;
+        }
     }
 
     //counts the number of patients with certain bloodtypes
-    public static ResultSet numBloodTypes(Connection con) throws SQLException {
+    //NEED THIS
+    public static String numBloodTypes(Connection con) throws SQLException {
         PreparedStatement bloodTypes = con.prepareStatement("SELECT BloodType, COUNT(*) FROM MedicalRecord GROUP BY BloodType");
         ResultSet results = bloodTypes.executeQuery();
-        while (results.next()) {
-            String bloodType = results.getString("BloodType");
-            int count = results.getInt("COUNT(*)");
-
-            System.out.println(bloodType + " | " + count);
+        String result = "Blood Type | Count\n";
+        if(!results.isBeforeFirst()) {
+            return "Error: No data found";
         }
-        return results;
+        else {
+            while (results.next()) {
+                String bloodType = results.getString("BloodType");
+                int count = results.getInt("COUNT(*)");
+                result += bloodType + " | " + count;
+            }
+        }
+        return result;
     }
 
     //shows the name, department, patient name, and patient diagnosis for every nurse in the hospital
-    public static ResultSet NursesPatients(Connection con) throws SQLException {
+    //NEED THIS
+    public static String NursesPatients(Connection con) throws SQLException {
         PreparedStatement nursePatients = con.prepareStatement("SELECT N.FullName, N.DeptName, p.PatientName, M.CurrentDiagnosis FROM nurses as N INNER JOIN Patients AS p ON N.PatientID = p.PatientID INNER JOIN MedicalRecord AS M ON P.PatientID = M.PatientID");
         ResultSet results = nursePatients.executeQuery();
-        System.out.println("Nurse Name | Department | Patient Name | Patient Diagnosis");
-        while (results.next()) {
-            String nurse = results.getString("N.FullName");
-            String department = results.getString("N.DeptName");
-            String patientName = results.getString("p.PatientName");
-            String patientDiagnosis = results.getString("M.CurrentDiagnosis");
-
-            System.out.println(nurse + " | " + department + " | " + patientName + " | " + patientDiagnosis);
+        String result = "Nurse Name | Department | Patient Name | Patient Diagnosis\n";
+        if(!results.isBeforeFirst()) {
+            return "Error: No matching data";
         }
-        return results;
+        else {
+            while (results.next()) {
+                String nurse = results.getString("N.FullName");
+                String department = results.getString("N.DeptName");
+                String patientName = results.getString("p.PatientName");
+                String patientDiagnosis = results.getString("M.CurrentDiagnosis");
+
+                result += nurse + " | " + department + " | " + patientName + " | " + patientDiagnosis + " \n";
+            }
+            return result;
+        }
+
     }
 
     //retrieve names and doctorids of doctors working on a given day
-    public static ResultSet doctorsWorking(Connection con, String date) throws SQLException {
+    //NEED THIS
+    public static String doctorsWorking(Connection con, String date) throws SQLException {
         Date patientDOB = new Date(date);
         java.sql.Date sqlDate = new java.sql.Date(patientDOB.getTime());
         PreparedStatement docsWorking = con.prepareStatement("SELECT Schedule.StartTime, Doctors.DoctorID, Doctors.FullName FROM (Doctors JOIN Schedule ON Doctors.DoctorID = Schedule.DoctorID) WHERE Schedule.Date = ?" +
                 " GROUP BY Schedule.StartTime, Doctors.DoctorID, Doctors.FullName");
         docsWorking.setDate(1, sqlDate);
         ResultSet results = docsWorking.executeQuery();
+        String result = "Start Time | DoctorID | Full Name\n";
 
-        while(results.next()) {
-            java.sql.Time startTime = results.getTime("Schedule.StartTime");
-            String doctorID = results.getString("Doctors.DoctorID");
-            String fullName = results.getString("Doctors.FullName");
-
-            System.out.println(startTime + " | " + doctorID + " | " + fullName);
+        if(!results.isBeforeFirst()) {
+            return "Error: No matching data";
         }
+        else {
+            while(results.next()) {
+                java.sql.Time startTime = results.getTime("Schedule.StartTime");
+                String doctorID = results.getString("Doctors.DoctorID");
+                String fullName = results.getString("Doctors.FullName");
 
-        return results;
+                result += startTime + " | " + doctorID + " | " + fullName + " \n";
+            }
+        }
+        return result;
     }
 
     //retrieve doctors with a certain specialization, grouped by years in specialization
-    public static ResultSet docSpecialization(Connection con, String specialization) throws SQLException {
+    //NEED THIS
+    public static String docSpecialization(Connection con, String specialization) throws SQLException {
         PreparedStatement getDocSpecialization = con.prepareStatement("Select Doctors.YrsExperience, Doctors.DoctorID, Doctors.FullName, COUNT(*) FROM (Doctors JOIN Specialization ON Doctors.DoctorID = Specialization.DoctorID) " +
                 "WHERE Specialization.Specialization = ? GROUP BY Doctors.YrsExperience, Doctors.DoctorID, Doctors.FullName");
         getDocSpecialization.setString(1, specialization);
         ResultSet results = getDocSpecialization.executeQuery();
 
-        while(results.next()) {
-            int yrsExp = results.getInt("Doctors.YrsExperience");
-            String docID = results.getString("Doctors.DoctorID");
-            String fName = results.getString("Doctors.FullName");
-            int count = results.getInt("COUNT(*)");
-
-            System.out.println(yrsExp + " | " + docID + " | " + fName + " | " + count);
+        String result = "Years Experience | Doctor ID | Full Name | Count\n";
+        if(!results.isBeforeFirst()) {
+            return "Error: No matching data";
         }
-        return results;
+        else {
+            while(results.next()) {
+                int yrsExp = results.getInt("Doctors.YrsExperience");
+                String docID = results.getString("Doctors.DoctorID");
+                String fName = results.getString("Doctors.FullName");
+                int count = results.getInt("COUNT(*)");
+
+                result += yrsExp + " | " + docID + " | " + fName + " | " + count;
+            }
+        }
+
+        return result;
     }
 
 }
